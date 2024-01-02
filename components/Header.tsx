94ff3a5db2fd83872ffc7313626544e91b11e432
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-01-01 19:39:26 -0800

    header, disney click, whitelist

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Header() {
   return (
-    <div>
-      
-    </div>
-  )
+    <header>
+      <Link href="/">
+        Click Me
+        <Image
+          src="https://links.papareact.com/a943ae"
+          alt="Disney Logo"
+          width={120}
+          height={100}
+          className="cursor-pointer invert"
+        />
+      </Link>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
