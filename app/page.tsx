3a116565ba04eb24f35dcd4b3f8286144b537806
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-01-06 15:31:24 -0800

    update with movieCards rendering

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,9 @@ export default async function Home() {
   return (
     <main className="">
       <div className="flex flex-col space-y-2 xl:-mt-48">
-        <MoviesCarousel movies={[]} title="Upcoming" />
+        <MoviesCarousel movies={upcomingMovies} title="Upcoming" />
+        <MoviesCarousel movies={topRatedMovies} title="Top Rated" />
+        <MoviesCarousel movies={popularMovies} title="Popular" />
       </div>
     </main>
   );
