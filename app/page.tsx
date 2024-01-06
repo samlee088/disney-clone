6b2994c36b9e1469ed1ac5698a0435c796061a6f
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-01-05 23:39:23 -0800

    fetch movies carousal data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import MoviesCarousel from "@/components/MoviesCarousel";
 import { Button } from "@/components/ui/button";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "@/lib/getMovies";
 import Image from "next/image";
 
 export default async function Home() {
   const upcomingMovies = await getUpcomingMovies();
-  const topRatedMovies = await gettopRatedMovies();
+  const topRatedMovies = await getTopRatedMovies();
   const popularMovies = await getPopularMovies();
 
   return (
--- lib/getMovies.ts
+++ lib/getMovies.ts
@@ -1,7 +1,46 @@
+import { SearchResults } from "@/typings";
+
 async function fetchFromTMDB(url: URL, cacheTime?: number) {
   url.searchParams.set("include_adult", "false");
   url.searchParams.set("include_video", "false");
   url.searchParams.set("sort_by", "popularity.desc");
   url.searchParams.set("language", "en-US");
   url.searchParams.set("page", "1");
+
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+    },
+    next: {
+      revalidate: cacheTime || 60 * 60 * 24,
+    },
+  };
+
+  const request = await fetch(url.toString(), options);
+  const data = (await request.json()) as SearchResults;
+
+  return data;
+}
+
+export async function getUpcomingMovies() {
+  const url = new URL("https://api.themoviedb.org/3/movie/upcoming");
+  const data = await fetchFromTMDB(url);
+
+  return data.results;
+}
+
+export async function getTopRatedMovies() {
+  const url = new URL("https://api.themoviedb.org/3/movie/top_rated");
+  const data = await fetchFromTMDB(url);
+
+  return data.results;
+}
+
+export async function getPopularMovies() {
+  const url = new URL("https://api.themoviedb.org/3/movie/popular");
+  const data = await fetchFromTMDB(url);
+
+  return data.results;
 }
