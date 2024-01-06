b804c646974c0c5a113eafa77c71dba4b2363934
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-01-05 22:56:14 -0800

    api calls, movies carosel outline

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedMovies = await gettopRatedMovies();
+  const popularMovies = await getPopularMovies();
+
   return (
     <main className="">
-      <h1>Building a disney clone application</h1>
-      <Button>Click Me</Button>
+      <div className="flex flex-col space-y-2 xl:-mt-48">
+        <MoviesCarousel movies={[]} title="Upcoming" />
+      </div>
     </main>
   );
 }
