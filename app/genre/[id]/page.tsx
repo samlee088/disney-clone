ff6f58db1e0db18c469570f38a32df1158f68259
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-01-09 18:51:20 -0800

    update with genre page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,3 +1,6 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
+
 type Props = {
   params: {
     id: string;
@@ -7,12 +10,15 @@ type Props = {
   };
 };
 
-const GenrePage = ({ params: { id }, searchParams: { genre } }: Props) => {
+async function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
+  const movies = await getDiscoverMovies(id);
   return (
-    <div>
-      Welcome to the genre with ID: {id} and name: {genre}
+    <div className="max-w-7xl mx-auto">
+    <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+      <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+    </div>
     </div>
   );
-};
+}
 
 export default GenrePage;
--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -20,8 +20,9 @@ async function SearchPage({ params: { term } }: Props) {
   const popularMovies = await getPopularMovies();
 
   return (
-    <div>
-      <div>
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10"> Results for {termToUse}</h1>
         <MoviesCarousel title="Movies" movies={movies} isVertical />
         <MoviesCarousel title="You may also like" movies={popularMovies} />
       </div>
